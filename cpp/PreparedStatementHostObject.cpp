070534922bfe13ae9ee08e3a657ce2b14617a6df
Abhishek Ranjan <abhishekranjan@Abhisheks-Mac-mini.local>
2025-06-11 22:54:13 +0530

    fix:fixed the building error in eas

--- cpp/PreparedStatementHostObject.cpp
+++ cpp/PreparedStatementHostObject.cpp
@@ -23,135 +23,204 @@ jsi::Value PreparedStatementHostObject::get(jsi::Runtime &rt,
     auto name = propNameID.utf8(rt);
 
     if (name == "bind") {
-        return HOSTFN("bind") {
-            if (_stmt == nullptr) {
-                throw std::runtime_error("statement has been freed");
-            }
+        return jsi::Function::createFromHostFunction(
+            rt, jsi::PropNameID::forAscii(rt, "bind"), 0,
+            [this](jsi::Runtime &rt, const jsi::Value &this_val,
+                   const jsi::Value *args, size_t count) -> jsi::Value {
+                if (_stmt == nullptr) {
+                    throw std::runtime_error("statement has been freed");
+                }
 
-            const jsi::Value &js_params = args[0];
-            std::vector<JSVariant> params = to_variant_vec(rt, js_params);
+                const jsi::Value &js_params = args[0];
+                std::vector<JSVariant> params = to_variant_vec(rt, js_params);
 
-            auto promiseCtr = rt.global().getPropertyAsFunction(rt, "Promise");
-      auto promise = promiseCtr.callAsConstructor(
-          rt, HOSTFN("executor") {
-                auto resolve = std::make_shared<jsi::Value>(rt, args[0]);
-                auto reject = std::make_shared<jsi::Value>(rt, args[1]);
-                auto task = [&rt, this, resolve, reject,
-                             invoker = this->_js_call_invoker, params]() {
-                    try {
+                auto promiseCtr =
+                    rt.global().getPropertyAsFunction(rt, "Promise");
+                auto promise = promiseCtr.callAsConstructor(
+                    rt, jsi::Function::createFromHostFunction(
+                            rt, jsi::PropNameID::forAscii(rt, "executor"), 0,
+                            [this, params](jsi::Runtime &rt,
+                                           const jsi::Value &this_val,
+                                           const jsi::Value *args,
+                                           size_t count) -> jsi::Value {
+                                auto resolve =
+                                    std::make_shared<jsi::Value>(rt, args[0]);
+                                auto reject =
+                                    std::make_shared<jsi::Value>(rt, args[1]);
+                                auto task = [this, &rt, resolve, reject,
+                                             params]() {
+                                    try {
 #ifdef OP_SQLITE_USE_LIBSQL
-                        opsqlite_libsql_bind_statement(_stmt, &params);
+                                        opsqlite_libsql_bind_statement(
+                                            _stmt, &params);
 #else
-                        opsqlite_bind_statement(_stmt, &params);
+                                        opsqlite_bind_statement(_stmt,
+                                                                &params);
 #endif
-                        invoker->invokeAsync([&rt, resolve] {
-                            resolve->asObject(rt).asFunction(rt).call(rt, {});
-                        });
-                    } catch (const std::runtime_error &e) {
-                        invoker->invokeAsync([&rt, e, reject] {
-                            auto errorCtr =
-                                rt.global().getPropertyAsFunction(rt, "Error");
-                            auto error = errorCtr.callAsConstructor(
-                                rt, jsi::String::createFromUtf8(rt, e.what()));
-                            reject->asObject(rt).asFunction(rt).call(rt, error);
-                        });
-                    } catch (const std::exception &e) {
-                        invoker->invokeAsync([&rt, e, reject] {
-                            auto errorCtr =
-                                rt.global().getPropertyAsFunction(rt, "Error");
-                            auto error = errorCtr.callAsConstructor(
-                                rt, jsi::String::createFromUtf8(rt, e.what()));
-                            reject->asObject(rt).asFunction(rt).call(rt, error);
-                        });
-                    }
-                };
+                                        _js_call_invoker->invokeAsync(
+                                            [this, &rt, resolve] {
+                                                resolve->asObject(rt)
+                                                    .asFunction(rt)
+                                                    .call(rt, {});
+                                            });
+                                    } catch (const std::runtime_error &e) {
+                                        _js_call_invoker->invokeAsync(
+                                            [this, &rt, e, reject] {
+                                                auto errorCtr =
+                                                    rt.global()
+                                                        .getPropertyAsFunction(
+                                                            rt, "Error");
+                                                auto error =
+                                                    errorCtr
+                                                        .callAsConstructor(
+                                                            rt,
+                                                            jsi::String::
+                                                                createFromUtf8(
+                                                                    rt,
+                                                                    e.what()));
+                                                reject->asObject(rt)
+                                                    .asFunction(rt)
+                                                    .call(rt, error);
+                                            });
+                                    } catch (const std::exception &e) {
+                                        _js_call_invoker->invokeAsync(
+                                            [this, &rt, e, reject] {
+                                                auto errorCtr =
+                                                    rt.global()
+                                                        .getPropertyAsFunction(
+                                                            rt, "Error");
+                                                auto error =
+                                                    errorCtr
+                                                        .callAsConstructor(
+                                                            rt,
+                                                            jsi::String::
+                                                                createFromUtf8(
+                                                                    rt,
+                                                                    e.what()));
+                                                reject->asObject(rt)
+                                                    .asFunction(rt)
+                                                    .call(rt, error);
+                                            });
+                                    }
+                                };
 
-                _thread_pool->queueWork(task);
+                                _thread_pool->queueWork(task);
 
-                return {};
-          }));
-      return promise;
-        });
+                                return {};
+                            }));
+                return promise;
+            });
     }
 
     if (name == "bindSync") {
-      return HOSTFN("bindSync") {
-        if (_stmt == nullptr) {
-          throw std::runtime_error("statement has been freed");
-        }
-        
-        const jsi::Value &js_params = args[0];
-        std::vector<JSVariant> params = to_variant_vec(rt, js_params);
-        try {
+        return jsi::Function::createFromHostFunction(
+            rt, jsi::PropNameID::forAscii(rt, "bindSync"), 0,
+            [this](jsi::Runtime &rt, const jsi::Value &this_val,
+                   const jsi::Value *args, size_t count) -> jsi::Value {
+                if (_stmt == nullptr) {
+                    throw std::runtime_error("statement has been freed");
+                }
+
+                const jsi::Value &js_params = args[0];
+                std::vector<JSVariant> params = to_variant_vec(rt, js_params);
+                try {
 #ifdef OP_SQLITE_USE_LIBSQL
-          opsqlite_libsql_bind_statement(_stmt, &params);
+                    opsqlite_libsql_bind_statement(_stmt, &params);
 #else
-          opsqlite_bind_statement(_stmt, &params);
+                    opsqlite_bind_statement(_stmt, &params);
 #endif
-        } catch (const std::runtime_error &e) {
-          throw std::runtime_error(e.what());
-        } catch (const std::exception &e) {
-          throw std::runtime_error(e.what());
-        }
-        return {};
-      });
+                } catch (const std::runtime_error &e) {
+                    throw std::runtime_error(e.what());
+                } catch (const std::exception &e) {
+                    throw std::runtime_error(e.what());
+                }
+                return {};
+            });
     }
 
     if (name == "execute") {
-        return HOSTFN("execute") {
-            if (_stmt == nullptr) {
-                throw std::runtime_error("statement has been freed");
-            }
+        return jsi::Function::createFromHostFunction(
+            rt, jsi::PropNameID::forAscii(rt, "execute"), 0,
+            [this](jsi::Runtime &rt, const jsi::Value &this_val,
+                   const jsi::Value *args, size_t count) -> jsi::Value {
+                if (_stmt == nullptr) {
+                    throw std::runtime_error("statement has been freed");
+                }
 
-            auto promiseCtr = rt.global().getPropertyAsFunction(rt, "Promise");
-        auto promise = promiseCtr.callAsConstructor(rt, HOSTFN("executor") {
-                auto resolve = std::make_shared<jsi::Value>(rt, args[0]);
-                auto reject = std::make_shared<jsi::Value>(rt, args[1]);
+                auto promiseCtr =
+                    rt.global().getPropertyAsFunction(rt, "Promise");
+                auto promise = promiseCtr.callAsConstructor(
+                    rt,
+                    jsi::Function::createFromHostFunction(
+                        rt, jsi::PropNameID::forAscii(rt, "executor"), 0,
+                        [this](jsi::Runtime &rt, const jsi::Value &this_val,
+                               const jsi::Value *args,
+                               size_t count) -> jsi::Value {
+                            auto resolve =
+                                std::make_shared<jsi::Value>(rt, args[0]);
+                            auto reject =
+                                std::make_shared<jsi::Value>(rt, args[1]);
 
-                auto task = [&rt, this, resolve, reject,
-                             invoker = this->_js_call_invoker]() {
-                    std::vector<DumbHostObject> results;
-                    std::shared_ptr<std::vector<SmartHostObject>> metadata =
-                        std::make_shared<std::vector<SmartHostObject>>();
-                    try {
+                            auto task = [this, &rt, resolve, reject]() {
+                                std::vector<DumbHostObject> results;
+                                std::shared_ptr<std::vector<SmartHostObject>>
+                                    metadata = std::make_shared<
+                                        std::vector<SmartHostObject>>();
+                                try {
 #ifdef OP_SQLITE_USE_LIBSQL
-                        auto status =
-                            opsqlite_libsql_execute_prepared_statement(
-                                _db, _stmt, &results, metadata);
+                                    auto status =
+                                        opsqlite_libsql_execute_prepared_statement(
+                                            _db, _stmt, &results, metadata);
 #else
-                        auto status = opsqlite_execute_prepared_statement(
-                            _db, _stmt, &results, metadata);
+                                    auto status =
+                                        opsqlite_execute_prepared_statement(
+                                            _db, _stmt, &results, metadata);
 #endif
-                        invoker->invokeAsync(
-                            [&rt, status = std::move(status),
-                             results =
-                                 std::make_shared<std::vector<DumbHostObject>>(
-                                     results),
-                             metadata, resolve] {
-                                auto jsiResult = create_result(
-                                    rt, status, results.get(), metadata);
-                                resolve->asObject(rt).asFunction(rt).call(
-                                    rt, std::move(jsiResult));
-                            });
-                    } catch (std::exception &exc) {
-                        invoker->invokeAsync([&rt, &exc, reject] {
-                            auto errorCtr =
-                                rt.global().getPropertyAsFunction(rt, "Error");
-                            auto error = errorCtr.callAsConstructor(
-                                rt,
-                                jsi::String::createFromUtf8(rt, exc.what()));
-                            reject->asObject(rt).asFunction(rt).call(rt, error);
-                        });
-                    }
-                };
+                                    _js_call_invoker->invokeAsync([&rt,
+                                                                   status = std::move(
+                                                                       status),
+                                                                   results =
+                                                                       std::make_shared<
+                                                                           std::vector<
+                                                                               DumbHostObject>>(
+                                                                           results),
+                                                                   metadata,
+                                                                   resolve] {
+                                        auto jsiResult =
+                                            create_result(rt, status,
+                                                          results.get(),
+                                                          metadata);
+                                        resolve->asObject(rt)
+                                            .asFunction(rt)
+                                            .call(rt, std::move(jsiResult));
+                                    });
+                                } catch (std::exception &exc) {
+                                    _js_call_invoker->invokeAsync(
+                                        [this, &rt, &exc, reject] {
+                                            auto errorCtr =
+                                                rt.global()
+                                                    .getPropertyAsFunction(
+                                                        rt, "Error");
+                                            auto error =
+                                                errorCtr.callAsConstructor(
+                                                    rt,
+                                                    jsi::String::
+                                                        createFromUtf8(
+                                                            rt, exc.what()));
+                                            reject->asObject(rt)
+                                                .asFunction(rt)
+                                                .call(rt, error);
+										});
+                                }
+                            };
 
-                _thread_pool->queueWork(task);
+                            _thread_pool->queueWork(task);
 
-                return {};
-          }));
+                            return {};
+                        }));
 
-        return promise;
-        });
+                return promise;
+            });
     }
 
     return {};
--- cpp/macros.h
+++ cpp/macros.h
@@ -1,15 +1,13 @@
 #pragma once
 
-#define HOSTFN(name)                                                           \
-jsi::Function::createFromHostFunction( \
-rt, \
-jsi::PropNameID::forAscii(rt, name), \
-0, \
-[this](jsi::Runtime &runtime, const jsi::Value &this_val, const jsi::Value *args, size_t count) -> jsi::Value
-
 #define HOST_STATIC_FN(name)                                                   \
 jsi::Function::createFromHostFunction( \
 rt, \
 jsi::PropNameID::forAscii(rt, name), \
 0, \
 [&](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *args, size_t count) -> jsi::Value
+
+#define JS_THROW_IF_NOT(rt, cond, msg)                                         \
+  if (!(cond)) {                                                               \
+    throw jsi::JSError(rt, msg);                                               \
+  }
